691b7de940a62e04e89b14bec2cb9a3f57c51d38
pvfigueiredo <paulo.figueiredo86@gmail.com>
2021-05-26 00:34:07 -0300

    remoção de imports desnecessários

--- src/api/routes/reports.py
+++ src/api/routes/reports.py
@@ -1,9 +1,7 @@
-from flask import Blueprint, request
+from flask import Blueprint
 from sqlalchemy import func
-from src.api.utils.responses import response_with
 from src.api.utils import responses as resp
 from src.api.models.employees import Employee, EmployeeSchema
-from src.api.utils.database import db
 
 reports_route = Blueprint("reports_route", __name__)
 
